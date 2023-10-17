dbd9a8c09926c9fc0da42936ba1f10700d5b0ff3
Te1ne <nq2019thaigiabao230504@gmail.com>
2023-10-17 21:58:43 +0700

    xong layout

---
+++ DoAn/Controllers/CategoryController.cs
@@ -0,0 +1,38 @@
+﻿using DoAn.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DoAn.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        SimenEntities db = new  SimenEntities();
+        public ActionResult Index()
+        {
+            return View(db.Categories.ToList());
+        }
+        public ActionResult Create()
+        {
+            Category cate = new Category();
+            return View(cate);
+        }
+        [HttpPost]
+        public ActionResult Create(Category cate)
+        {
+            try
+            {
+                db.Categories.Add(cate);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return Content("sai roi bro");
+            }
+        }
+    }
+}
---
+++ DoAn/Controllers/ProductController.cs
@@ -0,0 +1,38 @@
+﻿using DoAn.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DoAn.Controllers
+{
+    public class ProductController : Controller
+    {
+        SimenEntities db = new SimenEntities();
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View(db.Products.ToList());
+        }
+        public ActionResult Create()
+        {
+            Product pro = new Product();
+            return View(pro);
+        }
+        [HttpPost]
+        public ActionResult Create(Product pro)
+        {
+            try
+            {
+                db.Products.Add(pro);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+              return Content ("sai roi bro");
+            }
+        }
+    }
+}
